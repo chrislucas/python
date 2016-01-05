5db587540f260c0bf671d97e81702227314b29eb
christoffer <christoffer.luccas@gmail.com>
2016-01-05 16:59:08 -0200

    estudo de classes com teste de implementacao de uma estrutura de dados que suporte algoritmo de UNIO-FIND

---
+++ StudyClasses/study/classes/StudyClasses.py
@@ -0,0 +1,32 @@
+'''
+Created on Jan 5, 2016
+
+@author: christoffer
+'''
+ 
+'''
+In python, a namespace is a mapping from names to objects. Most namespaces are currently
+implemented as Python dictionaries.
+Examples of namespaces are: the set of built-in names (containing functions such as abs(), and
+built-in exception names); the global names in a module, and the local names in a function invocation.
+The impportant thing to know about namespaces is that there is abosolutely no relation
+between names in different namespaces. For example, two modules may both define a function 'X'
+without confusion
+'''
+
+# union find
+class UF:
+    elements = []
+    
+    def __init__(self):
+        return
+    
+    def root(self):
+        return
+    
+    def find(self):
+        return
+
+
+if __name__ == '__main__':
+    pass
