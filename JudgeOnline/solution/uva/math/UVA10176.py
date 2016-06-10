eb5d8abbde2aea6d6a1d3632e674bc453badeb68
wiselabs <christoffer.luccas@gmail.com>
2016-06-10 09:34:15 -0300

    commit

--- JudgeOnline/solution/uva/math/UVA10176.py
+++ JudgeOnline/solution/uva/math/UVA10176.py
@@ -1,19 +1,9 @@
 '''
-Created on 6 de jun de 2016
+Created on 10 de jun de 2016
 
 @author: C.Lucas
-
-https://www.urionlinejudge.com.br/judge/pt/problems/view/1398
-Resolvi em java. Nao consigo lidar com a entrada
-AGORA DONE
-
-Fatoracao em primos
-https://en.wikipedia.org/wiki/Fermat_primality_test
-http://stackoverflow.com/questions/12756335/fast-prime-factorization-algorithm
-https://en.wikipedia.org/wiki/Primality_test
-
-Number Theory - 1
-https://www.hackerearth.com/notes/number-theory-1/
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1117
+DONE
 '''
 from sys import stdin, stdout
 
