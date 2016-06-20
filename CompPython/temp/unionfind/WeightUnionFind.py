f29191ebdcb9aa4daedaf469dda47cd828e2a0c6
lucaschris <crhistoffer.luccas@gmail.com>
2016-06-20 19:24:56 -0300

    estudo uniopnfind

--- CompPython/temp/unionfind/WeightUnionFind.py
+++ CompPython/temp/unionfind/WeightUnionFind.py
@@ -1,59 +1,18 @@
 '''
-Created on Jun 3, 2016
+Created on 20 de jun de 2016
 
-@author: christoffer
+@author: chrislucas
 '''
 
-'''
-Em quickuinion
-id[idx] e o NO paid de idx
-id[] = [0, 1, 9, 4, 9, 6, 6, 7, 8, 9]
-
-NO pai de 2 eh o 9
-id[2] = 9 e id[9] = 9
- O NO pai de idx eh encontrado da seguinte forma
- 
-id[ id[ id[ id[idx] ] ] ] -> uma busca em profundidade ate q idx == id[idx]
-
-para saber se P e Q tem o mesmo no pai, basta veriticar se
-pai(p) == pai(q)
-
-A uniao de P e Q eh feita da seguinte forma'
-
-defina a raiz de P com o valor da Raiz de Q
-raizP = pai(P)
-rauzQ = pai(Q)
-id[raizP] = raizQ
-
-'''
-
-class Quickunion:
+class WeightUnionFind(object):
     '''
     classdocs
     '''
-    id = []
-    def __init__(self, n):
+    def __init__(self, params):
         '''
         Constructor
         '''
-        self.id = [x for x in range(0, n)]
-        None
-    
-    def root(self, idx):
-        while idx != self.id[idx]:
-            idx = self.id[idx]
-        return idx
-    
-    def isConnected(self, p, q):
-        rp = root(p) 
-        rq = root(q)
-        return rp == rq
-    
-    def union(self, p, q):
-        rp = root(p)
-        rq = root(q)
-        self.id[rp] = rq
-        
+
 '''
 Weight Quick Union tem uma estragegia diferente na funcao
 union. Para evitar que um no pai fique com muitos no filhos
@@ -86,25 +45,25 @@ class WeightQuickUnion:
     def root(self, idx):
         if idx == self.id[idx]:
             return idx
-        root(self, idx)
+        self.root(self, idx)
     
     def union(self, p, q):
-        rootP = int(p)
-        rootQ = int(q)
+        rootP = self.root(p)
+        rootQ = self.root(q)
         if rootP == rootQ:
             return None
         # se o n P tem mais nos associados a ele do que NO Q
-        elif(he[rootP] > he[rootQ]):
+        elif(self.he[rootP] > self.he[rootQ]):
             id[rootQ] = rootP           # o NO Q tera sua raiz modificada para raiz de P
-            he[rootP] += he[rootQ]      # o pesso da raiz Q eh adicionado a raiz P
+            self.he[rootP] += self.he[rootQ]      # o pesso da raiz Q eh adicionado a raiz P
         
         else:
             id[rootP] = rootQ
-            he[rootQ] += he[rootP]
+            self.he[rootQ] += self.he[rootP]
     
     def isConnected(self, p, q):
-        rp = root(p) 
-        rq = root(q)
+        rp = self.root(p) 
+        rq = self.root(q)
         return rp == rq
     
     def infoData(self):
@@ -114,11 +73,7 @@ def runTestWeightUF():
     ufw = WeightQuickUnion(10)
     ufw.union(1, 2)
     ufw.union(0, 1)
+    ufw.union(3, 2)
     ufw.infoData()
     
-runTestWeightUF()
-
-
-
-if __name__ == '__main__':
-    pass
+runTestWeightUF()      
