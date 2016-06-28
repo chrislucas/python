47070b2d66e112e7113b151737ea80f6cd37ca79
lucaschris <crhistoffer.luccas@gmail.com>
2016-06-28 19:41:18 -0300

    commit

---
+++ JudgeOnline/solution/math/JurosCompostos.py
@@ -0,0 +1,62 @@
+'''
+Created on 28 de jun de 2016
+
+@author: chrislucas
+
+http://brasilescola.uol.com.br/matematica/juros-compostos.htm
+http://www.somatematica.com.br/emedio/finan3.php
+http://fazaconta.com/juros-compostos.htm
+'''
+from sys import stdout
+
+'''
+M = C * (1 + TAXA) ^ TEMPO
+M / (1 + TAXA) ^ TEMPO = C
+'''
+
+def juros_comp(capital, taxa,  n):
+    montante = capital * ((1 + taxa) ** n)
+    return montante
+
+# JUROS = Montante - Principal
+def juros(capital, taxa,  n):
+    return capital * ((1 + taxa) ** n) - capital
+
+'''
+  Calcule o montante de um capital de R$6.000,00,
+  aplicado a juros compostos, durante 1 ano, a taxa de 3,5% ao mes.
+'''
+
+stdout.write("%f %f\n%f\n" % (
+     juros_comp(6000.0, 3.5/100, 12)
+    ,juros(6000.0, 3.5/100, 12)
+    ,juros_comp(7000.0, 1.5/100, 12)
+    )
+)
+
+
+'''
+Calcule o valor do capital que, aplicado a uma taxa de 2% ao mes,
+rendeu em 10 meses a quantia de R$ 15.237,43?
+'''
+stdout.write("%f\n" % (15237.43 / ((1 + 2/100) ** 10) ))
+
+'''
+Qual a taxa de juros empregada sobre o capital de 
+R$ 8.000,00 durante 12 meses que gerou o montante de R$ 10.145,93?
+'''
+
+'''
+http://brasilescola.uol.com.br/matematica/juros-compostos.htm
+M = C * (1 + TAXA) ^ TEMPO
+M / C = (1 + X) ^ 12
+D = (1 + X) ^ 12
+D ^ (1/12) = (1 + X)
+D ^ (1/12) - 1 = X
+'''
+stdout.write("%f\n" % ( ((10145.93/8000)**(1/12))-1 ) )
+
+
+
+if __name__ == '__main__':
+    pass
--- JudgeOnline/solution/ucoder/math/MinimumWage.py
+++ JudgeOnline/solution/ucoder/math/MinimumWage.py
@@ -5,6 +5,53 @@ Created on 22 de jun de 2016
 
 https://ucoder.com.br/problems/1267/
 '''
+from sys import stdin, stdout
+
+def calcTest(months, receivePerHour, hourPerMonth, per):
+    return months * receivePerHour * hourPerMonth * per
+
+def valor_futuro(capital, tax, time):
+    return capital * ((1 + tax) ** time)
+
+'''
+M = C * ( (1 + TAX) ^ TIME)
+M = 17627.81
+TAX (17627.81/C) ^ (1/TIME) - 1
+'''
+'''
+c = calcTest(3, 120, 160, .3)
+d = ((17627.81/c) ** (1/3))-1
+print("%f %f %f\n" % (c, valor_futuro(c, 1/100, 3), d)  )
+'''
+
+def calc (months, receivePerHour, hourPerMonth):
+    return months * receivePerHour * hourPerMonth
+
+def run():
+    data = [float(x) for x in stdin.readline().split(" ")]
+    ePerHourBrazil      = data[0]
+    ePerHourForeign     = data[1]
+    workingHours        = data[2]
+    DPB                 = data[3]
+    DPE                 = data[4]
+    CT                  = data[5]
+    
+    calcBr = calc(3, ePerHourBrazil, workingHours)
+    calcBr = calcBr - (calcBr * (DPB)/100) 
+    calcFg = calc(3, ePerHourForeign, workingHours)
+    calcFg = calcFg - (calcFg * (DPE)/100) 
+    tax = 1/100
+    valBr = valor_futuro(calcBr, tax, 3)
+    valFg = valor_futuro(calcFg, tax, 3)
+    
+    stdout.write("%.2f %.2f" % (valBr, valFg) )
+
+'''
+100.00 31.00 120.00 70.00 85.00 4.03
+120.00 27.00 160.00 70.00 71.00 4.60
+'''
+
+run()
 
 if __name__ == '__main__':
     pass
