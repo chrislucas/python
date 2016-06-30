9d23256347511df5f2fad8367117d68b2f0e7b3e
wiselabs <christoffer.luccas@gmail.com>
2016-06-29 22:44:22 -0300

    commit

---
+++ CompPython/sort/MergeKSortedArrays.py
@@ -0,0 +1,13 @@
+'''
+Created on 29 de jun de 2016
+
+@author: C.Lucas
+'''
+
+'''
+http://www.geeksforgeeks.org/merge-k-sorted-arrays/
+https://en.wikipedia.org/wiki/K-Way_Merge_Algorithms
+'''
+
+if __name__ == '__main__':
+    pass
