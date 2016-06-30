776dc472637f5b6d7765972a169b3e55c9452c1f
lucaschris <crhistoffer.luccas@gmail.com>
2016-06-30 19:25:39 -0300

    commit

--- JudgeOnline/solution/ucoder/math/MinimumWage.py
+++ JudgeOnline/solution/ucoder/math/MinimumWage.py
@@ -8,29 +8,33 @@ https://ucoder.com.br/problems/?q=&contest_text=V+MARATONA+DE+PROGRAMA%C3%87%C3%
 https://ucoder.com.br/problems/?q=&contest_text=IV+MARATONA+DE+PROGRAMA%C3%87%C3%83O+INTERFATECs+2015
 https://ucoder.com.br/problems/?q=&contest_text=Interfatecs+2014+1%C2%AA+fase
 https://ucoder.com.br/problems/?q=&contest_text=Interfatecs+2014+2%C2%AA+fase
+
+DONE
 '''
 from sys import stdin, stdout
 
 def calcTest(months, receivePerHour, hourPerMonth, per):
     return months * receivePerHour * hourPerMonth * per
 
-def valor_futuro(capital, tax, time):
-    return capital * ((1 + tax) ** time)
-
-'''
-M = C * ( (1 + TAX) ^ TIME)
-M = 17627.81
-TAX (17627.81/C) ^ (1/TIME) - 1
-'''
-'''
-c = calcTest(3, 120, 160, .3)
-d = ((17627.81/c) ** (1/3))-1
-print("%f %f %f\n" % (c, valor_futuro(c, 1/100, 3), d)  )
-'''
-
 def calc (months, receivePerHour, hourPerMonth):
     return months * receivePerHour * hourPerMonth
 
+def Test():
+    R = valor_futuro(5760, 0.01, 3) + valor_futuro(5760, 0.01, 2) + valor_futuro(5760, 0.01, 1)
+    print(R)
+
+def valor_futuro(capital, tax, time):
+    return capital * ((1 + tax) ** time)
+
+def sol(val, tax):
+    return valor_futuro(val, tax, 3) + valor_futuro(val, tax, 2) + valor_futuro(val, tax, 1)
+
+def sol2(val, tax, time):
+    _sum = 0.0
+    for i in range(time, 0, -1):
+        _sum += valor_futuro(val, tax, i)
+    return _sum
+        
 def run():
     data = [float(x) for x in stdin.readline().split(" ")]
     ePerHourBrazil      = data[0]
@@ -40,15 +44,12 @@ def run():
     DPE                 = data[4]
     CT                  = data[5]
     
-    calcBr = calc(3, ePerHourBrazil, workingHours)
-    calcBr = calcBr - (calcBr * (DPB)/100) 
-    calcFg = calc(3, ePerHourForeign, workingHours)
-    calcFg = calcFg - (calcFg * (DPE)/100) 
-    tax = 1/100
-    valBr = valor_futuro(calcBr, tax, 3)
-    valFg = valor_futuro(calcFg, tax, 3)
+    calcBr = ePerHourBrazil * workingHours * ((100 - DPB) / 100)
+    calcFg = ePerHourForeign * workingHours * ((100 - DPE) / 100)
+    valBr = sol2(calcBr, 0.01, 3)
+    valFg = sol2(calcFg, 0.01, 3)
     
-    stdout.write("%.2f %.2f" % (valBr, valFg) )
+    stdout.write("%.2fBR %.2fES" % (valBr, valFg * CT) )
 
 '''
 100.00 31.00 120.00 70.00 85.00 4.03
