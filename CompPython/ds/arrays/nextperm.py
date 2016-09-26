06269f9259b36124d0c804f0bee4d5a282bd1f22
lucaschris <crhistoffer.luccas@gmail.com>
2016-09-26 17:46:11 -0300

    novo problema

---
+++ CompPython/ds/arrays/nextperm.py
@@ -0,0 +1,14 @@
+'''
+Created on 26 de set de 2016
+
+@author: chrislucas
+'''
+
+'''
+Problem
+https://www.hackerrank.com/challenges/bigger-is-greater
+https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+'''
+
+if __name__ == '__main__':
+    pass
