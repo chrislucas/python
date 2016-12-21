07f89f5492e087dd8d0a0324c57b38be1367b254
Christoffer Lucas <christoffer.luccas@gmail.com>
2016-12-21 19:16:38 -0200

    commit

---
+++ JudgeOnline/solution/hrank/math/combinatory/LucasTheorem.py
@@ -0,0 +1,9 @@
+'''
+Created on 21 de dez de 2016
+
+@author: christoffer
+http://www.geeksforgeeks.org/compute-ncr-p-set-2-lucas-theorem/
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ JudgeOnline/solution/hrank/math/combinatory/nCrHR.py
@@ -0,0 +1,11 @@
+'''
+Created on 21 de dez de 2016
+
+@author: christoffer
+https://www.hackerrank.com/challenges/ncr
+https://pt.wikipedia.org/wiki/Teorema_de_Lucas
+https://en.wikipedia.org/wiki/Lucas's_theorem
+'''
+
+if __name__ == '__main__':
+    pass
